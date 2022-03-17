9444846b8223ec580d1b5039606fb05f05f8722b
Anupam <anupamvns0099@gmail.com>
2022-03-17 22:21:46 +0530

    removed unused files and changes

--- erpnext/regional/united_arab_emirates/utils.py
+++ erpnext/regional/united_arab_emirates/utils.py
@@ -1,10 +1,12 @@
-from __future__ import unicode_literals
+
 import frappe
 from frappe import _
-import erpnext
-from frappe.utils import flt, round_based_on_smallest_currency_fraction, money_in_words
-from erpnext.controllers.taxes_and_totals import get_itemised_tax
+from frappe.utils import flt, money_in_words, round_based_on_smallest_currency_fraction
 from six import iteritems
+
+import erpnext
+from erpnext.controllers.taxes_and_totals import get_itemised_tax
+
 
 def update_itemised_tax_data(doc):
 	if not doc.taxes: return
@@ -113,7 +115,6 @@ def update_totals(vat_tax, base_vat_tax, doc):
 			doc.outstanding_amount = doc.rounded_total or doc.grand_total
 
 	doc.in_words = money_in_words(doc.grand_total, doc.currency)
-	doc.project_amount_write = money_in_words(doc.project_amount, doc.currency)
 	doc.base_in_words = money_in_words(doc.base_grand_total, erpnext.get_company_currency(doc.company))
 	doc.set_payment_schedule()
 
